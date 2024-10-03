dd6e9b323db6b845e326ef1cc229719ec189387d
geovanaa24 <geovana.slv.crds@gmail.com>
2024-10-03 15:22:50 +0000

    iniciado o codigo javascript

---
+++ calculadora/js/script.js
@@ -0,0 +1,14 @@
+// FAZ REFERÊNCIA AOS ELEMENTOS DOM
+const numero1 = document.querySelector('#numero1')
+const numero2 = document.querySelector('#numero2')
+const btnCalcular = document.querySelector('#btnCalcular')
+const resultado = document.querySelector('#resultado')
+const adicao = document.querySelector('#adicao')
+const divisao = document.querySelector('#divisao')
+const subtracao = document.querySelector('#subtracao')
+const multiplicacao = document.querySelector('#multiplicacao')
+
+// CRIA A FUNÇÃO CALCULAR
+
+
+// ADICIONA ESCUTAR DE EVENTOS
