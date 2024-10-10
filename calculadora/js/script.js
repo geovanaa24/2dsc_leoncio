8282b935421c5e00f248d5a4c8cb9fe43eaffb99
geovanaa24 <geovana.slv.crds@gmail.com>
2024-10-10 15:24:04 +0000

    calcular numero

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -1,14 +1,29 @@
 // FAZ REFERÊNCIA AOS ELEMENTOS DOM
 const numero1 = document.querySelector('#numero1')
 const numero2 = document.querySelector('#numero2')
-const btnCalcular = document.querySelector('#btnCalcular')
-const resultado = document.querySelector('#resultado')
-const adicao = document.querySelector('#adicao')
-const divisao = document.querySelector('#divisao')
-const subtracao = document.querySelector('#subtracao')
-const multiplicacao = document.querySelector('#multiplicacao')
+const btnCalcular = document.querySelector('#btn-calcular')
+const resultado = document.querySelector('.resultado')
+const adicao = document.querySelector('.adicao')
+const divisao = document.querySelector('.divisao')
+const subtracao = document.querySelector('.subtracao')
+const multiplicacao = document.querySelector('.multiplicacao')
 
 // CRIA A FUNÇÃO CALCULAR
-
+function calcular(){
+    const n1 = Number (numero1.value)
+    const n2 = Number (numero2.value)
+    if(typeof n1 === 'number'
+        && typeof n2 === 'number'){
+            const adicao = `A soma de ${n1} e ${n2} = ${n1+n2}`
+            alert(adicao)
+        } else{
+            alert('Por favor, digite um número correto')
+        }
+    )
+//  alert('Resultado:' + resultado)
+}
 
 // ADICIONA ESCUTAR DE EVENTOS
+btnCalcular.addEventListener('click',function(){
+    calcular()
+})
